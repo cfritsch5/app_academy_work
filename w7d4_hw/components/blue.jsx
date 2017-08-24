0c1c7341c272b5cc2ec6f9908cc2275bdb3e2a3b
casey fritsch <caseyfritsch@gmail.com>
2017-08-24 16:51:22 -0700

    w7d4 hw

---
+++ w7d4_hw/components/blue.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Link, NavLink } from 'react-router-dom';
+import Indigo from './indigo';
+
+
+class Blue extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2 className="blue"></h2>
+        {/* Links here */}
+        <Link to='/blue' >Blue only</Link>
+        <Link to='/blue/indigo' >Add indigo</Link>
+
+        {/* Routes here */}
+      </div>
+    );
+  }
+}
+
+export default Blue;
